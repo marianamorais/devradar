a8968ec63241775291c385707bf6a9524a2ba13b
Mariana <marianamorais.dev@gmail.com>
2020-02-06 21:55:04 -0300

    Parametros

--- index.js
+++ index.js
@@ -2,7 +2,20 @@ const express = require('express');
 
 const app = express();
 
-app.get('/', (request, response) => {
+app.use(express.json());
+
+// Métodos HTTP: GET, POST, PUT, DELETE
+
+// Tipos  de parâmetros:
+
+//Query Params : request.query (Filtros, ordenação, paginação, ... )
+//Route Params: request.params (Identificar um recurso na alteração ou remoção)
+//Body: request.body (Dados para criação ou alteração de um registro)
+
+// MongoDB (Não-relacional)
+
+app.post('/users', (request, response) => {
+  console.log(request.query);
   return response.json({ message: 'Hello person'});
 });
 
